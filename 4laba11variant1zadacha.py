591faeb356bd8e4e8ede92a7a989cd988d0daeb9
Bogdan Madryga <113440162+bogdanmadryga@users.noreply.github.com>
2022-09-17 13:06:57 +0300

    Update 4laba11variant1zadacha.py

--- 4laba11variant1zadacha.py
+++ 4laba11variant1zadacha.py
@@ -1,6 +1,7 @@
 import math
-x = float(input("x ="))
-cr = math.fabs(x + 1) + 0.1
-korkub = cr ** (1./3.)
-h = math.tan(2*x) + math.cos(4*x-math.sqrt(x) - 2 / korkub)
-print(h)
+def skr(x):
+    h = math.tan(2 * x) + math.cos(4 * x - math.sqrt(x) - 2 / math.fabs(x + 1) + 0.1 ** (1. / 3.))
+    return h
+x1 = float(input("x ="))
+print(skr(x1))
+
