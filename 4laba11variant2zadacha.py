42f09c26c6d0073c13a011afb9085c3b51c7f8ce
Bogdan Madryga <113440162+bogdanmadryga@users.noreply.github.com>
2022-09-17 13:07:29 +0300

    Update 4laba11variant2zadacha.py

--- 4laba11variant2zadacha.py
+++ 4laba11variant2zadacha.py
@@ -1,5 +1,7 @@
 import math
-x = float(input("x ="))
-y = float(input("y ="))
-h = x * math.log10(y) + 1 / x ** 2 + y ** 2 + 0.03 - math.e ** 6 * x - y
-print(h)
+def skr(x, y):
+    h = x * math.log10(y) + 1 / x ** 2 + y ** 2 + 0.03 - math.e ** 6 * x - y
+    return h
+x1 = float(input("x ="))
+y1 = float(input("y ="))
+print(skr(x1, y1))
